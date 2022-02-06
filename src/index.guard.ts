76560b6eba58f450d4ca9a856600078c653eb823
Julian de Mourgues <julian.demourgues@mediengruppe-rtl-extern.de>
2022-02-06 16:29:07 +0100

    new strings

--- src/index.guard.ts
+++ src/index.guard.ts
@@ -2,18 +2,11 @@
  * Generated type guards for "index.d.ts".
  * WARNING: Do not manually change this file.
  */
-import { FileStr, File, TypescriptFile, OasFile, HttpsString, DomainEnding, UrlString } from "./index";
-
-export function isFileStr(obj: any, _argumentName?: string): obj is FileStr {
-    return (
-        typeof obj === "`file:${string}`"
-    )
-}
+import { File, TypescriptFile, OasFile, HttpsString, DomainEnding } from "./index";
 
 export function isFile(obj: any, _argumentName?: string): obj is File {
     return (
         (typeof obj === "string" ||
-            isFileStr(obj) as boolean ||
             (obj !== null &&
                 typeof obj === "object" ||
                 typeof obj === "function") &&
@@ -26,11 +19,6 @@ export function isFile(obj: any, _argumentName?: string): obj is File {
 export function isTypescriptFile(obj: any, _argumentName?: string): obj is TypescriptFile {
     return (
         (typeof obj === "string" &&
-            (obj !== null &&
-                typeof obj === "object" ||
-                typeof obj === "function") &&
-            obj.fileType === "ts" ||
-            isFileStr(obj) as boolean &&
             (obj !== null &&
                 typeof obj === "object" ||
                 typeof obj === "function") &&
@@ -51,13 +39,6 @@ export function isTypescriptFile(obj: any, _argumentName?: string): obj is Types
 export function isOasFile(obj: any, _argumentName?: string): obj is OasFile {
     return (
         (typeof obj === "string" &&
-            (obj !== null &&
-                typeof obj === "object" ||
-                typeof obj === "function") &&
-            (obj.fileType === "yaml" ||
-                obj.fileType === "yml" ||
-                obj.fileType === "json") ||
-            isFileStr(obj) as boolean &&
             (obj !== null &&
                 typeof obj === "object" ||
                 typeof obj === "function") &&
@@ -88,11 +69,5 @@ export function isHttpsString(obj: any, _argumentName?: string): obj is HttpsStr
 export function isDomainEnding(obj: any, _argumentName?: string): obj is DomainEnding {
     return (
         null
-    )
-}
-
-export function isUrlString(obj: any, _argumentName?: string): obj is UrlString {
-    return (
-        typeof obj === "`https://${string}.${any}`"
     )
 }
